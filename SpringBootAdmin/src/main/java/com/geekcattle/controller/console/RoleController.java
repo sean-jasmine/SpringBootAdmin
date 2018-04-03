a119c4c16bc53fa50cf5128211b65727130d05ca
tianjuns <15095647760@163.com>
2018-04-03 18:04:28 +0800

    Update RoleController.java

--- SpringBootAdmin/src/main/java/com/geekcattle/controller/console/RoleController.java
+++ SpringBootAdmin/src/main/java/com/geekcattle/controller/console/RoleController.java
@@ -1,7 +1,3 @@
-/*
- * Copyright (c) 2017 <l_iupeiyu@qq.com> All rights reserved.
- */
-
 package com.geekcattle.controller.console;
 
 import com.geekcattle.model.console.*;
@@ -31,10 +27,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
-/**
- * author geekcattle
- * date 2016/10/21 0021 下午 15:58
- */
+
 @Controller
 @RequestMapping("/console/role")
 public class RoleController {
