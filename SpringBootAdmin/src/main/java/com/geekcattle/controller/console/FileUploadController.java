cd07d482d613295b59effc6091c20a62db661c0c
tianjuns <15095647760@163.com>
2018-04-03 18:01:39 +0800

    Update FileUploadController.java

--- SpringBootAdmin/src/main/java/com/geekcattle/controller/console/FileUploadController.java
+++ SpringBootAdmin/src/main/java/com/geekcattle/controller/console/FileUploadController.java
@@ -1,7 +1,3 @@
-/*
- * Copyright (c) 2017 <l_iupeiyu@qq.com> All rights reserved.
- */
-
 package com.geekcattle.controller.console;
 
 import org.slf4j.Logger;
@@ -9,10 +5,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-/**
- * author geekcattle
- * date  2017/5/24 0024 下午 3:40.
- */
+
 @Controller
 @RequestMapping("/console/upload")
 public class FileUploadController {
